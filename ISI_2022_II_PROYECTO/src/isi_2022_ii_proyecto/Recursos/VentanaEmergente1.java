d2c520b9a5c0548e34176d458d212a39a6a45532
Edwin Rafael <edwin.espino@ujcv.edu>
2022-07-20 03:13:32 -0600

    VENTANAS EMERGENTES1

--- ISI_2022_II_PROYECTO/src/isi_2022_ii_proyecto/Recursos/VentanaEmergente1.java
+++ ISI_2022_II_PROYECTO/src/isi_2022_ii_proyecto/Recursos/VentanaEmergente1.java
@@ -108,6 +108,7 @@ public class VentanaEmergente1 extends javax.swing.JFrame {
 
     private void rSButtonRound1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rSButtonRound1ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_rSButtonRound1ActionPerformed
 
     /**
